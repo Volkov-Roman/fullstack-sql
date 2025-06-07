0d7f530161f63485c61ec5f7af54a31567096783
Roman Volkov <rvolkovpost@gmail.com>
2025-06-07 14:57:25 +0300

    13.5

---
+++ controllers/blogs.js
@@ -0,0 +1,50 @@
+const router = require('express').Router()
+
+const { Blog } = require('../models')
+
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id)
+  next()
+}
+
+router.get('/', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+router.get('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    res.json(req.blog)
+  } else {
+    res.status(404).end()
+  }
+})
+
+router.post('/', async (req, res) => {
+  try {
+    console.log(req.body)
+    const blog = await Blog.create(req.body)
+    res.json(blog)
+  } catch (error) {
+    res.status(400).json({ error })
+  }
+})
+
+router.put('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    req.blog.likes = req.body.likes
+    await req.blog.save()
+    res.json(req.blog)
+  } else {
+    res.status(404).end()
+  }
+})
+
+router.delete('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    await req.blog.destroy()
+  }
+  res.status(204).end()
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,76 +1,20 @@
-require('dotenv').config()
-const { Sequelize, Model, DataTypes } = require('sequelize')
 const express = require('express')
 const app = express()
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
+const { PORT } = require('./util/config')
+const { connectToDatabase } = require('./util/db')
 
-class Blog extends Model {}
+const blogsRouter = require('./controllers/blogs')
 
-Blog.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  author: {
-    type: DataTypes.TEXT
-  },
-  url: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  title: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  likes: {
-    type: DataTypes.INTEGER,
-    defaultValue: 0
-  }
-}, {
-  sequelize,
-  underscored: true,
-  timestamps: false,
-  modelName: 'blog'
-})
+app.use(express.json())
 
-Blog.sync()
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', async (req, res) => {
-  const blogs = await Blog.findAll()
-  res.json(blogs)
-})
+const start = async () => {
+  await connectToDatabase()
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+}
 
-app.get('/api/blogs/:id', async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id)
-  if (blog) {
-    res.json(blog)
-  } else {
-    res.status(404).end()
-  }
-})
-
-app.post('/api/blogs', async (req, res) => {
-  try {
-    console.log(req.body)
-    const blog = await Blog.create(req.body)
-    res.json(blog)
-  } catch (error) {
-    res.status(400).json({ error })
-  }
-})
-
-app.delete('/api/blogs/:id', async (req, res) => {
-  const deleted = await Blog.destroy({ where: { id: req.params.id } })
-  if (deleted) {
-    res.status(204).end()
-  } else {
-    res.status(404).json({ error: 'blog not found' })
-  }
-})
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+start()
