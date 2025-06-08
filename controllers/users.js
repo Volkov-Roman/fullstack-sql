496ba38da46d57f2bc5fe08ad846c004df16f5ce
Roman Volkov <rvolkovpost@gmail.com>
2025-06-08 14:21:49 +0300

    13.17

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,15 @@
+const { tokenExtractor } = require('../util/middleware')
 const router = require('express').Router()
 
 const { User, Blog } = require('../models')
+
+const isAdmin = async (req, res, next) => {
+  const user = await User.findByPk(req.decodedToken.id)
+  if (!user.admin) {
+    return res.status(401).json({ error: 'operation not allowed' })
+  }
+  next()
+}
 
 router.get('/', async (req, res) => {
   const users = await User.findAll({
@@ -30,19 +39,35 @@ router.get('/:id', async (req, res) => {
   }
 })
 
-router.put('/:username', async (req, res, next) => {
-  try {
-    const user = await User.findOne({ where: { username: req.params.username } })
-    if (!user) {
-      return res.status(404).json({ error: 'user not found' })
+router.put('/:username', tokenExtractor, isAdmin, async (req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: req.params.username
     }
+  })
 
-    user.username = req.body.username
+  if (user) {
+    user.disabled = req.body.disabled
     await user.save()
     res.json(user)
-  } catch (error) {
-    next(error)
+  } else {
+    res.status(404).end()
   }
 })
+
+// router.put('/:username', async (req, res, next) => {
+//   try {
+//     const user = await User.findOne({ where: { username: req.params.username } })
+//     if (!user) {
+//       return res.status(404).json({ error: 'user not found' })
+//     }
+
+//     user.username = req.body.username
+//     await user.save()
+//     res.json(user)
+//   } catch (error) {
+//     next(error)
+//   }
+// })
 
 module.exports = router
